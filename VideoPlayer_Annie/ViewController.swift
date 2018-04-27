88ce03ae3a06339a40dd95034c18c0d37dd18741
王安妮 <pore0814@gmail.com>
2018-04-27 14:12:25 +0800

    play video

--- VideoPlayer_Annie/ViewController.swift
+++ VideoPlayer_Annie/ViewController.swift
@@ -7,19 +7,52 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    var playerItem : AVPlayerItem!
+    var videoPlayer : AVPlayer!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        }
+       
+ 
+    @IBAction func btn_Search(_ sender: Any) {
+        
+                let url = "https://s3-ap-northeast-1.amazonaws.com/mid-exam/Video/taeyeon.mp4"
+                if let url = URL(string: url){
+                    print(url)
+                    let player = AVPlayer(url: url)
+                    let playerLayer = AVPlayerLayer(player: player)
+                    playerLayer.frame = self.view.bounds
+                    self.view.layer.addSublayer(playerLayer)
+                    player.play()
+        
+        }
+        
     }
+    
+func Search(_ sender: Any) {
+    
+  print("aaaa")
+    
+//
+//        let url = "https://s3-ap-northeast-1.amazonaws.com/mid-exam/Video/taeyeon.mp4"
+//        if let url = URL(string: url){
+//            print(url)
+//            let player = AVPlayer(url: url)
+//            let playerLayer = AVPlayerLayer(player: player)
+//            playerLayer.frame = self.view.bounds
+//            self.view.layer.addSublayer(playerLayer)
+//            player.play()
+//
+//
+    
+        
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-
-
+    
 }
 
